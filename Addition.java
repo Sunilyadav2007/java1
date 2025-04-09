4ac1ab19cc715464da2dbc1e00b8e44130a88be5
Sunilyadav2007 <sujangandoli@gmail.com>
2025-04-09 08:53:50 +0530

    this is subdtraction

--- Addition.java
+++ Addition.java
@@ -3,6 +3,8 @@ public static void main(String[] args) {
     int a =10;
     int b =5;
     int sum = a+b;
+    int sub = a-b;
     System.out.println("the sum is:"+sum);
+    System.out.println("the sub is:"+sub);
 }
 }
